af20ab3fe70c57d4ee123b42a351eb5fa344b620
GenKnot <167164229+GenKnot@users.noreply.github.com>
2024-10-22 12:17:11 -0400

    Updated Teams

--- pages/teams/index.tsx
+++ pages/teams/index.tsx
@@ -241,8 +241,8 @@ const Teams: NextPage = () => {
 
                     <div className="text-center mb-12">
                         <h3 className="display-3 px-xl-10">我们的组织架构图</h3>
-                        <a href="/img/MIEC/teams/MIEC3.svg" data-glightbox data-gallery="g1">
-                            <img src="/img/MIEC/teams/MIEC3.svg" alt="SVG Icon"
+                        <a href="/img/MIEC/teams/MIEC4.svg" data-glightbox data-gallery="g1">
+                            <img src="/img/MIEC/teams/MIEC4.svg" alt="SVG Icon"
                                  style={{width: '100%', height: 'auto'}}/>
                         </a>
                     </div>
